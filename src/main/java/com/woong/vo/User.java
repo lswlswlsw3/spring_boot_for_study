409bfa5682e64e552c4afd3cbc87dad04f4e86e3
DESKTOP-2FMNJGO\Woongs <lswlswlsw3@gmail.com>
2018-10-15 11:00:17 +0900

    initial commit

---
+++ src/main/java/com/woong/vo/User.java
@@ -0,0 +1,38 @@
+package com.woong.vo;
+
+public class User {
+	private String userId;
+	private String userPassword;
+	private String name;
+	private String email;
+
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userPassword=" + userPassword + ", name=" + name + ", email=" + email
+				+ "]";
+	}
+}
---
+++ src/main/java/com/woong/web.java
@@ -0,0 +1,5 @@
+package com.woong;
+
+public class web {
+
+}
